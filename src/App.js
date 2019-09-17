b52f4106ad4c296b3556412eecec504ce21a01df
alfrantzell <Arne.Frantzell@gmail.com>
2019-09-16 19:07:48 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faSun as sun,
@@ -12,126 +11,116 @@ import {
     faCloudSun as partlyCloudyDay,
     faCloudMoon as partlyCloudyNight
 } from '@fortawesome/free-solid-svg-icons'
-var momentTZ = require('moment-timezone')
+import momentTZ from 'moment-timezone'
+import moment from 'moment'
+
 momentTZ().tz("America/Los-Angeles").format();
-var moment = require('moment')
 moment().format();
 
-
 //component for hourly forecast
-class Hourly extends Component{
+const Hourly = ({tern, hourlyData}) => {
+  
+    //Convert the hourly forecast object into an array-like object with Object.entries
+    //Filter object for 6 forecasts at 2-hour intervals, then map over it
+    //Use moments.js to convert the UNIX timestamps for each forecast into hh format, adjusted to the user's timezone
+    const forecastHours = Object.entries(hourlyData).filter((el, index)=>{
+        return !(index%2) && index<12
+    }).map((el, index) => {
+            const icon = el[1].icon
+            const timeZ = Intl.DateTimeFormat().resolvedOptions().timeZone
+            const date = moment.unix(el[1].time).tz(timeZ).format('hA')
+    
+            //use tern function created upstream in WeatherData component
+            //to determine which icon is displayed for each hour
+            return (
+            <div>  
+                <p>{date}</p>
+                <p><FontAwesomeIcon icon = {tern(icon)}/></p>
+                <p>{Math.round(el[1].temperature)}°F</p>
+            </div>
+            );
+    }) 
 
-render(){
+    return (
+        <div class = "container">  
+        {forecastHours}
+        </div>
+    )
 
-let tern = this.props.tern   
-
-//First, convert the hourly forecast object into an array-like object with Object.entries
-//Second, map over the object to return 6 forecasts at 2-hour intervals
-//Third, use moments.js to convert the UNIX timestamps for each forecast into hh format, adjusted to the user's timezone
-let forecastHours = Object.entries(this.props.object.hourly.data).map(function(el, index){
-    if(index%2===0 && index<12){
-        let icon = el[1].icon
-        let date = moment.unix(el[1].time)
-        let timeZ = Intl.DateTimeFormat().resolvedOptions().timeZone
-        date = date.tz(timeZ).format('hA')
-
-        //use tern function created upstream in WeatherData component
-        //to determine which icon is displayed for each hour
-        return (<div>  
-                    <p>{date}</p>
-                    <p><FontAwesomeIcon icon = {tern(icon)}/></p>
-                    <p>{Math.round(el[1].temperature)}°F</p>
-                </div>) 
-                }
-        }) 
-
-return (
-    <div class = "container">  
-    {forecastHours}
-    </div>
-)
-
-} }
-
+}
 
 //component for error handling
-class Error extends Component{
-    render(){
-        return(
+const Error = () => (
             <div>
                 <br />
                 <i>Location not found.</i>
             </div>
-        )
-    }
-}
+)
 
 //component to return all weather data
-class WeatherData extends Component{
-    
-    render(){
+const WeatherData = ({forecast, summary}) => {
         //if the sun icon is displayed, color it yellow
         //all other icons remain white
-        var iconColor
-        if (this.props.object.currently.icon === "clear-day"){
+        let iconColor = "white"
+        if(forecast.currently.icon === "clear-day"){
             iconColor = "yellow"
-            }
-        else{iconColor = "white"}
+        }
 
-        var icon = this.props.object.currently.icon    
+        const icon = forecast.currently.icon    
         
-        //reusable function for determining which fontawesome icon to display
-        var tern = function(icono){
+        //determine which fontawesome icon to display
+        const tern = (icono) => {
             if(icono === "clear-day"){ return sun}
-            else if(icono === "clear-night"){ return moon}
-            else if(icono === "rain"){return rain}
-            else if(icono === "snow" || icono === "sleet"){ return snow}
-            else if (icono === "wind"){return wind}
-            else if (icono === "cloudy" || icono === "fog"){return cloud}
-            else if (icono === "partly-cloudy-day"){return partlyCloudyDay}
-            else if (icono === "partly-cloudy-night"){return partlyCloudyNight}}
+            if(icono === "clear-night"){ return moon}
+            if(icono === "rain"){return rain}
+            if(icono === "snow" || icono === "sleet"){ return snow}
+            if(icono === "wind"){return wind}
+            if(icono === "cloudy" || icono === "fog"){return cloud}
+            if(icono === "partly-cloudy-day"){return partlyCloudyDay}
+            if(icono === "partly-cloudy-night"){return partlyCloudyNight}
+        }
 
         return(            
             <div>    
-            <p class = "med">{this.props.summary}</p>
+            <p class = "med">{summary}</p>
             <p><FontAwesomeIcon icon = {tern(icon)} 
                                         size="5x"
                                         color = {iconColor}/></p>
-            <p class = "huge">{Math.round(this.props.object.currently.temperature)}°F</p>
-            <p class = "highLow">H: {Math.round(this.props.object.daily.data[0].temperatureHigh)}°F
-                             L: {Math.round(this.props.object.daily.data[0].temperatureLow)}°F</p>
-            <Hourly object = {this.props.object}
+            <p class = "huge">{Math.round(forecast.currently.temperature)}°F</p>
+            <p class = "highLow">H: {Math.round(forecast.daily.data[0].temperatureHigh)}°F
+                             L: {Math.round(forecast.daily.data[0].temperatureLow)}°F</p>
+            <Hourly hourlyData = {forecast.hourly.data}
                     tern = {tern}/>
             </div>
         )
-    }
 }
 
+
 //component that selects error handler or weather data components based on geocoding API quality score
-class Results extends Component{   
-    render(){
+const Results = ({summary, forecast, quality}) => {   
 
-        //return nothing if no location given
-        if(this.props.summary === ""){
-            return null
-        }
-
-        //returns error if low MapQuest geocoding API quality score
-        if(this.props.quality.substring(2) === "XXX"){
-            return <Error />
-        }
-
-        //otherwise, return weather data component
-        return <div><WeatherData 
-                summary = {this.props.summary}
-                object = {this.props.object}
-                /></div>
+    //return nothing if no location given
+    if(summary === ""){
+        return null
     }
+
+    //returns error if low MapQuest geocoding API quality score
+    if(quality.substring(2) === "XXX"){
+        return <Error />
+    }
+
+    //otherwise, return weather data component
+    return <div><WeatherData 
+            summary = {summary}
+            forecast = {forecast}
+            />
+            </div>
+    
 }
 
 //component for user to input placename
-const SearchBar = props => (
-    <form onSubmit = {props.doIt}>      
+const SearchBar = ({userInput}) => (
+    <form onSubmit = {userInput}>      
         <input 
             type="text" 
             name = "entry" 
@@ -150,53 +139,47 @@ class App extends Component{
             lon: "",
             quality: "",
             summary: "",
-            object: {}
+            forecast: {}
         }
       
-    this.doIt = this.doIt.bind(this);
-    this.weather = this.weather.bind(this);       
+    this.userInput = this.userInput.bind(this);
     }
     
 //weather component calls the DarkSky API
 //the DarkSky API uses the latitude and longitude output from the MapQuest geocoding API as input to find weather
 //proxy is used to bypass CORS protection of DarkSky API    
-weather(){
-    const proxy = 'https://cors-anywhere.herokuapp.com/'
-    const darkAPI = `https://api.darksky.net/forecast/${process.env.REACT_APP_API_KEY}/${this.state.lat},${this.state.lon}`
-
-    fetch(proxy + darkAPI)
-    .then(response => response.json())
-    .then(data => this.setState({summary: data.currently.summary,
-                                object: data
-                                },  function(){console.log("darksky API: " + this.state.lat + " " + this.state.lon)}))
-}
                             
-doIt(e){
-    e.preventDefault()
-    const location = e.target.elements.entry.value
-    const mapAPI = `http://open.mapquestapi.com/geocoding/v1/address?key=${process.env.REACT_APP_MAPQUEST_KEY}&location=${location}`
+    userInput(e){
+        e.preventDefault()
+        const location = e.target.elements.entry.value
+        
+        //error handler for no value given
+        if (location.length === 0){
+            return null
+        }
     
-  //error handler for no value given
-  if (location.length === 0){
-      return null
-  }
+        const mapAPI = `http://open.mapquestapi.com/geocoding/v1/address?key=${process.env.REACT_APP_MAPQUEST_KEY}&location=${location}` 
+        //MapQuest geocoding API takes placename as user input and finds latitude and longitude   
+            fetch(mapAPI)
+                .then(response => response.json())
+                .then(data => {
+                    const {latLng, geocodeQualityCode} = (data.results[0].locations[0])
+                    this.setState({
+                        lat: latLng.lat, 
+                        lon: latLng.lng,
+                        quality: geocodeQualityCode
+                    })
+                }).then(()=> {
+                    const proxy = 'https://cors-anywhere.herokuapp.com/'
+                    const darkAPI = `https://api.darksky.net/forecast/${process.env.REACT_APP_API_KEY}/${this.state.lat},${this.state.lon}`
     
-  //MapQuest geocoding API takes placename as user input and finds latitude and longitude   
-    fetch(mapAPI)
-        .then(response => response.json())
-        .then(data=> this.setState({ lat: data.results[0].locations[0].latLng.lat, 
-                                   lon: data.results[0].locations[0].latLng.lng,
-                                    quality: data.results[0].locations[0].geocodeQualityCode
-                                   }, function(){console.log("mapquest API: quality:" + this.state.quality + ", " + this.state.location + ", " + this.state.lat + " " + this.state.lon)}))
+        return fetch(proxy + darkAPI)
+        }).then(response => response.json())
+        .then(data => this.setState({summary: data.currently.summary,
+                                    forecast: data
+                                    }))
     
-}
-
-//React lifecycle component used to call DarkSky API only after state is set with new latitude
-componentDidUpdate(prevProps, prevState){
-    if (this.state.lat !== prevState.lat) {
-      this.weather()
     }
-  }
     
   render(){
       
@@ -205,18 +188,30 @@ componentDidUpdate(prevProps, prevState){
           <br />
         
         <SearchBar 
-          doIt = {this.doIt}/>
+          userInput = {this.userInput}/>
           
         <Results
             quality = {this.state.quality}
             summary = {this.state.summary}
-            object = {this.state.object}
+            forecast = {this.state.forecast}
             />
         </div>
-          )
+        )
   
-}
+    }
           
-          }
+}
 
 export default App;
+
+//group imports together, separate function stuff
+//all arrow functions
+//correct indentation
+//use descriptive names for components
+//filter data before mapping over it
+//destructure props at the head of every component
+//destructure data in fetch method
+//join the two API calls
+          //use callback function to call first, use return to chain the 2nd call
+//use server-side app to handle darksky API request, rather than using the proxy
+//refactor app with react hooks 
